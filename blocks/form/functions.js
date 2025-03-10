6324b59c093136a2a9011f026acbca2bb751d296
Ravisanker E <ravisanker1999@gmail.com>
2025-03-10 13:06:01 +0530

    Update functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -28,5 +28,19 @@ function days(endDate, startDate) {
   return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
 }
 
+/** 
+
+ * Set the minimum date for a date field. 
+
+ * @param {date} date 
+
+ * @param {scope} globals 
+
+ */ 
+
+function setMinDate(date, globals){    
+  globals.functions.setProperty(globals.field, {minimum: date});
+} 
+
 // eslint-disable-next-line import/prefer-default-export
-export { getFullName, days };
+export { getFullName, days, setMinDate };
